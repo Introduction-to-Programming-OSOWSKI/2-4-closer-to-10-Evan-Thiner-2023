e7ebf25024370e3d375581ca10f55eb5b2e3ecd0
Evan-Thiner-2023 <817548@mystma.org>
2021-09-28 09:38:17 -0500

    another one

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
 def close10(x,y):
-    if abs(x)>10:
+    if x-y>10:
         return x
-    elif abs(y)>10:
+    elif y-x>10:
         return y
     else:
         return 0
-print(close10(5,5))
+print(close10(5,15))
